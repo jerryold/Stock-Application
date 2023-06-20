8ccc1166c5efed983d3bd0ff32ece5a7869b14da
unknown <jerryold@gmail.com>
2023-06-20 15:19:37 +0800

    time test

--- stocknotification.py
+++ stocknotification.py
@@ -60,7 +60,7 @@ def sendToLine():
 
 #設定特定時間執行
 # for i in ["09:00", "09:30", "10:00", "10:30", "11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","14:52","15:00","15:30"]:
-for i in ["03:14","03:18"]:
+for i in ["03:22","03:24 PM","03:26PM","03:28"]:
     schedule.every().monday.at(i).do(sendToLine)
     schedule.every().tuesday.at(i).do(sendToLine)
     schedule.every().wednesday.at(i).do(sendToLine)
@@ -74,4 +74,4 @@ for i in ["03:14","03:18"]:
 
 while True:
     schedule.run_pending()
-    time.sleep(30)
+    time.sleep(1)
