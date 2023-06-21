2dc72196d3f992c7aec882ec79e7f879e12e5e37
unknown <jerryold@gmail.com>
2023-06-21 09:41:22 +0800

    add time interval 9

--- stocknotification.py
+++ stocknotification.py
@@ -68,7 +68,7 @@ def sendToLine():
 
 def weekday_job(x):
     week = datetime.today().weekday()
-    if week<5:
+    if week<5 and datetime.now().hour >= 9:
         # schedule.every(30).minutes.until(timedelta(minutes=360)).do(x)
         schedule.every(30).seconds.until(timedelta(seconds=180)).do(x)
 
