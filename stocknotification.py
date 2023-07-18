00b7949b7bd0d7d1646ea6c597ce6130a68b7fd0
unknown <jerryold@gmail.com>
2023-07-18 09:33:45 +0800

    update AI Stock

--- stocknotification.py
+++ stocknotification.py
@@ -60,10 +60,10 @@ def sendToLine1(): #ETF
     payload1={'message':{msg00878,msg0056,msg0050,msg006208}} 
     headers = {'Authorization': 'Bearer ' + '2ewKp4GssKQH9zY2VAwj07WMKxWYj3Ij6n5j1S0vSxV'}
     response = requests.request("POST", url, headers=headers, data=payload1)
-    print(response.text)
+    # print(response.text)
 
    
-
+#半導體股
 def sendToLine2():
     
     stock2330 = twstock.realtime.get('2330')
@@ -104,7 +104,62 @@ def sendToLine2():
     payload1={'message':{msg2330,msg2454,msg2379,msg8299}} #台積電 and 聯發科 and 瑞昱 and 群聯message
     headers = {'Authorization': 'Bearer ' + '2ewKp4GssKQH9zY2VAwj07WMKxWYj3Ij6n5j1S0vSxV'}
     response = requests.request("POST", url, headers=headers, data=payload1)
-    print(response.text)
+    
+    # print(response.text)
+    
+def sendToLine3():#AI相關股票
+    
+    stock2356 = twstock.realtime.get('2356')
+    open2356=  stock2356['realtime']['open']
+    low2356 = stock2356['realtime']['low']
+    high2356 = stock2356['realtime']['high']
+    ltr2356 = stock2356['realtime']['latest_trade_price']   
+    msg2356=(f' \n 英業達 2356 \n||開盤價:{get_two_float(open2356, 2)} \n||最低價 {get_two_float(low2356, 2)} ||最高價 {get_two_float(high2356, 2)} \n||現價:{get_two_float(ltr2356, 2)} \n')
+    
+      
+
+    stock3231 = twstock.realtime.get('3231')
+    open3231=  stock3231['realtime']['open']
+    low3231 = stock3231['realtime']['low']
+    high3231 = stock3231['realtime']['high']
+    ltr3231 = stock3231['realtime']['latest_trade_price']
+    msg3231=(f' \n 緯創 3231 \n||開盤價:{get_two_float(open3231, 2)} \n||最低價:{get_two_float(low3231, 2)} ||最高價:{get_two_float(high3231, 2)} \n||現價:{get_two_float(ltr3231, 2)} \n')
+    
+
+    stock2382 = twstock.realtime.get('2382')
+    open2382= stock2382['realtime']['open']
+    low2382 = stock2382['realtime']['low']
+    high2382 = stock2382['realtime']['high']
+    ltr2382 = stock2382['realtime']['latest_trade_price']
+    msg2382=(f' \n 廣達 2382 \n||開盤價:{get_two_float(open2382, 2)} \n||最低價:{get_two_float(low2382, 2)} ||最高價:{get_two_float(high2382, 2)} \n||現價:{get_two_float(ltr2382, 2)} \n')
+    
+
+    stock4938 = twstock.realtime.get('4938')
+    open4938= stock4938['realtime']['open']
+    low4938 = stock4938['realtime']['low']
+    high4938 = stock4938['realtime']['high']
+    ltr4938 = stock4938['realtime']['latest_trade_price']
+    msg4938=(f' \n 和碩 4938 \n||開盤價:{get_two_float(open4938, 2)} \n||最低價:{get_two_float(low4938, 2)} ||最高價:{get_two_float(high4938, 2)} \n||現價:{get_two_float(ltr4938, 2)} \n')
+    
+    stock2324 = twstock.realtime.get('2324')
+    open2324= stock2324['realtime']['open']
+    low2324 = stock2324['realtime']['low']
+    high2324 = stock2324['realtime']['high']
+    ltr2324 = stock2324['realtime']['latest_trade_price']
+    msg2324=(f' \n 仁寶 2324 \n||開盤價:{get_two_float(open2324, 2)} \n||最低價:{get_two_float(low2324, 2)} ||最高價:{get_two_float(high2324, 2)} \n||現價:{get_two_float(ltr2324, 2)} \n')
+
+    stock3515 = twstock.realtime.get('3515')
+    open3515= stock3515['realtime']['open']
+    low3515 = stock3515['realtime']['low']
+    high3515 = stock3515['realtime']['high']
+    ltr3515 = stock3515['realtime']['latest_trade_price']
+    msg3515=(f' \n 華擎 3515 \n||開盤價:{get_two_float(open3515, 2)} \n||最低價:{get_two_float(low3515, 2)} ||最高價:{get_two_float(high3515, 2)} \n||現價:{get_two_float(ltr3515, 2)} \n')
+
+
+    url = "https://notify-api.line.me/api/notify"
+    payload1={'message':{msg2356,msg3231,msg2382,msg4938,msg2324,msg3515}} #AI相關股票
+    headers = {'Authorization': 'Bearer ' + '2ewKp4GssKQH9zY2VAwj07WMKxWYj3Ij6n5j1S0vSxV'}
+    response = requests.request("POST", url, headers=headers, data=payload1)
 
     
 
@@ -120,7 +175,8 @@ week = datetime.today().weekday()
 # Creating a scheduler object.
 scheduler = BlockingScheduler()
 scheduler.add_job(sendToLine1, "cron", minute='20',hour='7',day_of_week='mon-fri')
-scheduler.add_job(sendToLine2, "cron", minute='15,35,55',hour='1-6',day_of_week='mon-fri')
+scheduler.add_job(sendToLine2, "cron", minute='25,45',hour='1-6',day_of_week='mon-fri')
+scheduler.add_job(sendToLine3, "cron", minute='10,30,50',hour='1-6',day_of_week='mon-fri')
 # Starting the scheduler in a separate thread.
 scheduler.start()
       
